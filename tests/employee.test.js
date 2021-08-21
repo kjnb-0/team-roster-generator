9413370ec57c499e5e150f13393791ce4f923814
Krista Nielsen <kristanielsen@Kristas-MacBook-Air.local>
2021-08-21 16:38:26 -0700

    employee.test passing all tests w/ all 4 parameters

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -1,25 +1,20 @@
-//The first class is an `Employee` parent class with the following properties and methods:
-// * `getName()`
-
-// * `getId()`
-
-// * `getEmail()`
-
-// * `getRole()`&mdash;returns `'Employee'`
-
-const { describe } = require("yargs");
+const { expect } = require("@jest/globals");
 const Employee = require("../lib/employee");
 
 //test for creating new valid employee object
-
 describe("Employee", () => {
-  describe("getName", () => {
-    it("should save employee's name from user input", () => {
-      const employee = new Employee("ExampleName");
+
+  describe("initialize", () => {
+    it("should create new employee object with valid inputs of name, id, email, role", () => {
+      const employee = new Employee("ExampleName", 100, "example@example.com", "Employee");
       //make sure new object has correct properties
 
       expect(employee.name).toEqual("ExampleName");
+      expect(employee.id).toEqual(100)
+      expect(employee.email).toEqual("example@example.com")
+      expect(employee.role).toEqual("Employee")
     });
+
     //tests for errors 
     it("should err if left blank", () => {
         const cb = () => new Employee();
@@ -30,24 +25,26 @@ describe("Employee", () => {
         const err = new Error("Expected parameter 'name' to be a non-empty string");
         expect(cb).toThrowError(err);
       });
-  });
-  describe("getId", () => {
-    //test for creating new valid employee object
-    it("should save employee's name from user input", () => {
-      const employee = new Employee("ExampleName");
-      //make sure new object has correct properties
-
-      expect(employee.name).toEqual("ExampleName");
-    });
-    //tests for errors 
-    it("should err if left blank", () => {
-        const cb = () => new Employee();
-        expect(cb).toThrow();
+      it("should err if 'id' is negative number", () => {
+        const cb = () => new Employee("Martha", -1);
+        const err = new Error("Expected parameter 'id' to be a non-negative number");
+  
+        expect(cb).toThrowError(err);
       });
-      it("should err if 'name' is not a string", () => {
-        const cb = () => new Employee(10);
-        const err = new Error("Expected parameter 'name' to be a non-empty string");
+      it("should err if 'id' is not a number", () => {
+        const cb = () => new Employee("Martha");
+        const err = new Error("Expected parameter 'id' to be a non-negative number");
+        expect(cb).toThrowError(err);
+      });
+      it("should err if 'email' is not a string", () => {
+        const cb = () => new Employee("Martha",50,3);
+        const err = new Error("Expected parameter 'email' to be a non-empty string");
+        expect(cb).toThrowError(err);
+      });
+      it("should err if 'role' does not equal 'employee' ", () => {
+        const cb = () => new Employee("Martha",50,"email","customer");
+        const err = new Error("Expected parameter 'role' to equal 'employee'");
         expect(cb).toThrowError(err);
       });
   });
-});  
+  });  
