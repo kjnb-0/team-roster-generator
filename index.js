402ac67258b1dac173aef6d22ef1f1e48d52d5b5
Krista Nielsen <kristanielsen@Kristas-MacBook-Air.local>
2021-08-23 15:52:43 -0700

    after tutoring session, wrapping getinfo/writeinfo in functions, adding global variable array to store answers

--- index.js
+++ index.js
@@ -7,56 +7,96 @@ const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
 const Manager = require("./lib/manager");
 
-inquirer
-  .prompt([
-    {
-      type: "input",
-      message: "What is your name?",
-      name: "name",
-    },
-    {
-      type: "input",
-      message: "What is your ID?",
-      name: "id",
-    },
-    {
-      type: "input",
-      message: "What is your email?",
-      name: "email",
-    },
-    {
-      type: "list",
-      message: "What is your role?",
-      name: "role",
-      choices: ["Engineer","Intern","Manager","Employee"]
-    },
-  ])
-  .then((data) => {
-    const { name, id, email, role } = data;
+const employeeArray = []
 
-    let htmlContent = `<!DOCTYPE html>
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is your ID?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is your email?",
+        name: "email",
+      },
+      {
+        type: "list",
+        message: "What is your role?",
+        name: "role",
+        choices: ["Engineer", "Intern", "Manager"],
+      },
+    ])
+    .then(async (data) => {
+      //destructuring object
+      const { name, id, email, role } = data;
+      let specificInfo;
+      //different questions based on role choice
+      if (role === "Engineer") {
+        specificInfo = await inquirer.prompt([
+          { type: "input", message: "What is your Github?", name: "github" },
+        ]);
+        let newEmployee = new Engineer(name,id,email,specificInfo.github)
+        employeeArray.push(newEmployee);
+        console.log(newEmployee)
+      } else if (role === "Intern") {
+        specificInfo = await inquirer.prompt([
+          { type: "input", message: "What is your school?", name: "school" },
+        ]);
+      } else {
+        specificInfo = await inquirer.prompt([
+          {
+            type: "input",
+            message: "What is your office number?",
+            name: "officeNumber",
+          },
+        ]);
+      }
+      console.log(specificInfo);
+      let {add} = await inquirer.prompt([
+        { type: "list", message: "Add other employee?", name: "add", choices:["Yes","No"] },
+      ]);
+      console.log(add)
+      if (add === "Yes") {
+          addEmployee()
+      } else {
+          writeFile()
+      }
+    })}
+
+    
+    function writeFile() {
+        console.log(employeeArray)
+      let htmlContent = `<!DOCTYPE html>
     <html lang="en">
     <head>
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
       <title>Document</title>
-      <style>
-      html {
-          color: indigo;
-      }
-      </style>
+      <link rel="stylesheet" href="style.css">
     </head>
     <body>
-      <h1>My name is ${name}</h1>
-      <h2>My id is ${id}</h2>
-      <h3>My email is ${email}</h3>
-      <h4>My Role is ${role}</h4>
+     
     </body>
     </html>`;
 
-    const filename = path.join(__dirname, "html_output", "index.html");
+      const filename = path.join(__dirname, "dist", "index.html");
 
-    fs.writeFile(filename, htmlContent, (err) =>
-      err ? console.log(err) : console.log("Success!")
-    );
-  });
+      fs.writeFile(filename, htmlContent, (err) =>
+        err ? console.log(err) : console.log("Success!")
+      );
+    }
+
+addEmployee();
+
+{/* <h1>My name is ${name}</h1>
+<h2>My id is ${id}</h2>
+<h3>My email is ${email}</h3>
+<h4>My Role is ${role}</h4> */}
